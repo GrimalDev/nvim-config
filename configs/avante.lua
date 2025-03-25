e7c48c576fd37959ea8b8ae4e5fd25c09b8059c9
GrimalDev <gb.titou2b@gmail.com>
2025-03-25 15:31:10 +0100

    avante display names]

--- configs/avante.lua
+++ configs/avante.lua
@@ -13,12 +13,63 @@ return {
   },
   -- provider = "deepseek",
   provider = "claude",
+  claude = {
+    endpoint = "https://api.anthropic.com",
+    -- model = "claude-3-5-sonnet-latest",
+    model = "claude-3-5-haiku-latest",
+    thinking = {
+      type = "disabled",
+    },
+    disable_tools = true,
+  },
   vendors = {
     deepseek = {
       __inherited_from = "openai",
       api_key_name = "DEEPSEEK_API_KEY",
       endpoint = "https://api.deepseek.com",
-      model = "deepseek-coder",
+      model = "deepseek-reasoner",
+      disable_tools = true,
+    },
+    ["claude_37_tools_think"] = {
+      __inherited_from = "claude",
+      model = "claude-3-7-sonnet-latest",
+      display_name = "Claude 3.7 (Sonnet) with tools and thinking",
+      thinking = {
+        type = "enabled",
+        budget_tokens = 2048,
+      },
+      disable_tools = true,
+      temperature = 1,
+    },
+    ["claude_37"] = {
+      __inherited_from = "claude",
+      model = "claude-3-7-sonnet-latest",
+      display_name = "Claude 3.7 (Sonnet)",
+      thinking = {
+        type = "disabled",
+      },
+      disable_tools = true,
+    },
+    ["claude_haiku_tools"] = {
+      __inherited_from = "claude",
+      model = "claude-3-5-haiku-latest",
+      display_name = "Claude 3.5 (Haiku) with tools",
+      disable_tools = false,
+    },
+    ["claude_haiku"] = {
+      __inherited_from = "claude",
+      model = "claude-3-5-haiku-latest",
+      display_name = "Claude 3.5 (Haiku)",
+      disable_tools = true,
+    },
+  },
+  file_selector = {
+    provider_opts = {
+      get_filepaths = function(params)
+        local pwd = vim.fn.getcwd()
+        local cmd = string.format("fd --base-directory '%s' --hidden", vim.fn.fnameescape(pwd))
+        return vim.fn.systemlist(cmd)
+      end,
     },
   },
 }
