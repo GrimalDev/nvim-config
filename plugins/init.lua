2d321a1bbdb08b82974fd59000c20c6a4e169fa1
GrimalDev <gb.titou2b@gmail.com>
2025-05-16 10:29:19 +0200

    added blink nvchad integration

--- plugins/init.lua
+++ plugins/init.lua
@@ -1,18 +1,16 @@
 return {
   {
-		"nvim-tree/nvim-web-devicons",
-	},
+    "nvim-tree/nvim-web-devicons",
+  },
   {
     "ibhagwan/fzf-lua",
   },
   {
-		"nvim-telescope/telescope-fzf-native.nvim",
-		build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
-	},
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+  },
   {
     "nvim-lua/plenary.nvim",
   },
-  {
-		"nvim-lua/plenary.nvim",
-  },
+  { import = "nvchad.blink.lazyspec" },
 }
